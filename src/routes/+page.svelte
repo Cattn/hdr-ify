2f799271a3755845a1bdca9fb12ec17b33ed310c
Logan <Logan@cattn.dev>
2025-08-17 04:32:58 -0400

    fix color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,10 @@
     let color2 = $state('#00ff6f');
 </script>
 
-<h1 {@attach hdrifyHex('#ffff6f')}>Hdrify Hex: {color}</h1>
+<h1 {@attach hdrifyHex(color)}>Hdrify Hex: {color}</h1>
 
 <h2 style="color: {color2}" {@attach hdrify()}>Hdrify: {color2}</h2>
 
-<h3 {@attach hdrifyBackgroundHex('#00ff6f')}>Hdrify Background Hex: {color2}</h3>
+<h3 {@attach hdrifyBackgroundHex(color)}>Hdrify Background Hex: {color}</h3>
 
 <button onclick={() => color = '#ffff6f'}>Change text</button>
